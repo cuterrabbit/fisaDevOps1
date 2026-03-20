9520cde58b9109f76167cdd81d244ad691c52301
cuterrabbit <ledw8992@naver.com>
2026-03-20 15:23:20 +0900

    docs:자바오류수정

--- src/main/java/edu/fisa/ce/Controller.java
+++ src/main/java/edu/fisa/ce/Controller.java
@@ -9,7 +9,7 @@ public class Controller {
 	@GetMapping("ce1")
 	public String getData() {
 		System.out.println("요청 및 응답 .....");
-		System.out.println("두바퀴로 가는 자동차")
+		System.out.println("두바퀴로 가는 자동차");
 		return "성공";
 	}
 
